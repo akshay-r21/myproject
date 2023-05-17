4ab64b957a71d0432a573537529b8450af764cc2
akshay-r21 <akshay.ramachandran21@gmail.com>
2023-05-17 13:16:53 +0530

    Commit

--- bin/Operators.dart
+++ bin/Operators.dart
@@ -11,24 +11,26 @@ void main() {
   //int out = a/b;// this ids error bcoz res will be double and its  trying to store in int
   //create with var or dynamic
   //2 assignment ops += -= *= /= %=
-  print('a = b   a=${a = b}');//a=4
+  print('a = b   a=${a = b}'); //a=4
   print('a += b  a=${a + b}');
   print('a -= b  a=${a - b}');
   print('a /= b  a=${a / b}');
   print('a *= b  a=${a * b}');
   print('a %= b  a=${a % b}');
+  print('a %= b  a=${a % b}');
+  print('a %= b  a=${a % b}');
 
-   //3.Relational ops > < >= <=
-  int c =100,d= 200;
-  print('c>d = ${c>d}');
-  print('c>=d = ${c>=d}');
-  print('c<d = ${c<d}');
-  print('c<=d = ${c<=d}');
-  print('c!=d = ${c!=d}');
+  //3.Relational ops > < >= <=
+  int c = 100, d = 200;
+  print('c>d = ${c > d}');
+  print('c>=d = ${c >= d}');
+  print('c<d = ${c < d}');
+  print('c<=d = ${c <= d}');
+  print('c!=d = ${c != d}');
 
 //4. conditional ops && || !
-   String username='admin';
-   String password = "adminn";
+  String username = 'admin';
+  String password = "adminn";
 
   print(username == "asds" && password == "123");
   print(username == "admin" || password == "123");
